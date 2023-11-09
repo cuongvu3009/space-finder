e198ecf4f9b3d0af9d6242ef2101b0ec84405620
cuongvu3009 <cuongvu3009@gmail.com>
2023-11-09 21:22:52 +0200

    test: test query 2

--- test/space-finder.test.ts
+++ test/space-finder.test.ts
@@ -1,10 +1,13 @@
 import { handler } from "../services/spaces/handler";
 
+process.env.AWS_REGION = "eu-north-1";
+process.env.TABLE_NAME = "SpaceStack-062bf889427a";
+
 handler(
   {
     httpMethod: "POST",
     body: JSON.stringify({
-      location: "Esppoo",
+      location: "Dublin",
     }),
   } as any,
   {} as any
